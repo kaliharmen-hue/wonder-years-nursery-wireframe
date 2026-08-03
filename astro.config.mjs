e9bd4f47ec82eebaf4ad0579b4a97c7426a3b81a
kaliharmen-hue <kaliharmen@gmail.com>
2026-08-03 11:17:25 +0100

    Configure Astro for GitHub Pages

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://kaliharmen-hue.github.io',
+  base: '/wonder-years-nursery-wireframe',
+  output: 'static'
+});
